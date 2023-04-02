d5b492c241a59cd4f6eb65345259818b3f646df2
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-03 01:47:55 +0600

    Exercise: Creating a custom reusable button

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
@@ -6,6 +7,9 @@ function App() {
       <Alert>
         Hello <span>World</span>
       </Alert>
+      <Button color="success" onClick={() => console.log("Clicked the button")}>
+        Press me please
+      </Button>
     </div>
   );
 }
---
+++ mosh-react-course/src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  color?: "primary" | "success";
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
