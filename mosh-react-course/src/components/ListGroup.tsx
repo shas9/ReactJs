f915d4df5387551cfdb4d8f423dfe7867f2f6712
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-02 11:47:39 +0600

    Rendering for if else condition

--- mosh-react-course/src/components/ListGroup.tsx
+++ mosh-react-course/src/components/ListGroup.tsx
@@ -1,9 +1,14 @@
 function ListGroup() {
-  const items = ["New york", "San Francisco", "Tokyo", "Rajshahi", "London"];
+  let items = ["New york", "San Francisco", "Tokyo", "Rajshahi", "London"];
+  items = [];
 
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li className="list-group-item" key={item}>
