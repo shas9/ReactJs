a29b8833d8d68160fd4a4c1eeae254c9c9dc69a5
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-02 11:06:58 +0600

    Fragment with empty bracket

--- mosh-react-course/src/components/ListGroup.tsx
+++ mosh-react-course/src/components/ListGroup.tsx
@@ -1,8 +1,6 @@
-import { Fragment } from "react";
-
 function ListGroup() {
   return (
-    <Fragment>
+    <>
       <h1>List</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
@@ -11,7 +9,7 @@ function ListGroup() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth d one</li>
       </ul>
-    </Fragment>
+    </>
   );
 }
 
