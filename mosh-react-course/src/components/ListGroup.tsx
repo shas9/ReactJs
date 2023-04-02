da38015ec87379a3bd17190a9004445ee268ab30
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-02 11:14:43 +0600

    Rendering List

--- mosh-react-course/src/components/ListGroup.tsx
+++ mosh-react-course/src/components/ListGroup.tsx
@@ -1,13 +1,15 @@
 function ListGroup() {
+  const items = ["New york", "San Francisco", "Tokyo", "Rajshahi", "London"];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth d one</li>
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
