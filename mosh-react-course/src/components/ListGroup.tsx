406c6d8159235ee84edcae94ab19a2392df306a2
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-02 17:09:23 +0600

    Handling event

--- mosh-react-course/src/components/ListGroup.tsx
+++ mosh-react-course/src/components/ListGroup.tsx
@@ -1,17 +1,22 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New york", "San Francisco", "Tokyo", "Rajshahi", "London"];
-  items = [];
 
   const getMessage = () => {
     return items.length === 0 && <p>No item found</p>;
   };
+
+  //Event handler
+  const handleCLick = (event: MouseEvent) => console.log(event);
+
   return (
     <>
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item" key={item}>
+          <li className="list-group-item" key={item} onClick={handleCLick}>
             {item}
           </li>
         ))}
