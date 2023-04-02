4ddb87eb9b7817520864eea4f2d29f67b796c868
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-03 03:08:39 +0600

    Exercise: Alert

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+  const handleOnClick = () => {
+    setShowAlert(true);
+  };
+
+  const handleOnClose = () => {
+    setShowAlert(false);
+  };
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-      <Button color="success" onClick={() => console.log("Clicked the button")}>
+      {showAlert && (
+        <Alert onClose={handleOnClose}>
+          Hello <span>World</span>
+        </Alert>
+      )}
+      <Button color="success" onClick={handleOnClick}>
         Press me please
       </Button>
     </div>
