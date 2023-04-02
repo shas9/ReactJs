96e364907df3aaa71ae23af6de325355844ee46d
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-03 01:15:10 +0600

    Passing function via props

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["New york", "San Francisco", "Tokyo", "Rajshahi", "London"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- mosh-react-course/src/components/ListGroup.tsx
+++ mosh-react-course/src/components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   //Hook
   const [selectedInedx, setSelectedIndex] = useState(-1);
 
@@ -28,6 +29,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
