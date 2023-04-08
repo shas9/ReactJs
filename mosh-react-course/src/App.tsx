79eaa502ada85fba8eb89dceb1a56cc35f553836
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-08 16:05:58 +0600

    Adding icons

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
 import ListGroup from "./components/ListGroup";
+import { BsFillCalendarFill } from "react-icons/bs";
 
 function App() {
   const items = ["Dhaka", "Rajshahi", "Ctg"];
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Bangladesh"
-        onSelectItem={() => {}}
-      ></ListGroup>
+      <BsFillCalendarFill color="red" size={40} />
     </div>
   );
 }
