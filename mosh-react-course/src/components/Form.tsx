a227bfb21045c4561576446ef1edef66d6f24892
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-10 03:29:57 +0600

    Accessing input fields

--- mosh-react-course/src/components/Form.tsx
+++ mosh-react-course/src/components/Form.tsx
@@ -1,9 +1,18 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    if (nameRef.current && ageRef.current) {
+      person.name = nameRef.current.value;
+      person.age = parseInt(ageRef.current.value);
+
+      console.log(person);
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -11,13 +20,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-labl">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
