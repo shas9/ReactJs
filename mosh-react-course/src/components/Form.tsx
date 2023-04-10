7c37239ac28753fe243c9f81eca5f038b618d9a3
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-11 03:14:25 +0600

    Disabling a button

--- mosh-react-course/src/components/Form.tsx
+++ mosh-react-course/src/components/Form.tsx
@@ -15,10 +15,8 @@ const Form = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormData>({ resolver: zodResolver(schema) });
-
-  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit((data) => console.log(data))}>
@@ -46,7 +44,7 @@ const Form = () => {
         />
         {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
