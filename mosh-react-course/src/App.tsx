081bd129b87ee56d75b20850c7dd5a3bb7244ea0
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-12 03:24:11 +0600

    Integrating with React Hook Form and Zod

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -3,8 +3,7 @@ import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./expense-tracker/categories";
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
