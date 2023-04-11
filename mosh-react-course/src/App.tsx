2f58c0675a6d425e546b8aa1ddba3756580d7cad
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-12 03:05:47 +0600

    Building Expense Form

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -18,6 +21,9 @@ const App = () => {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
