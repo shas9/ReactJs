df88676fee75e9c8f6cdce8ce977faf15675bf23
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-12 02:47:26 +0600

    Building Expenses Filter

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10.03, category: "Utilities" },
@@ -10,10 +12,19 @@ const App = () => {
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id: number) =>
           setExpenses(expenses.filter((e) => e.id !== id))
         }
