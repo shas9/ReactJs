7acd8fdfd44412a73338ee880b6fedd882f81a6d
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-12 02:25:16 +0600

    Building expenses list

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10.03, category: "Utilities" },
+    { id: 13, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id: number) =>
+          setExpenses(expenses.filter((e) => e.id !== id))
+        }
+      />
     </div>
   );
 };
