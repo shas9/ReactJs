41cc71f00ff34614f8e412d41002dd6419eca5fe
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-15 03:23:36 +0600

    Using effect hook

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const ref = useRef<HTMLInputElement>(null);
+
+  //afterRender
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
+
+  return (
+    <div>
+      <input ref={ref} type="text" className="form-control" />
+    </div>
+  );
 };
 
 export default App;
