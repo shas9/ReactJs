73fea139b3afc32355231e25d28af3fb903a6b5b
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-15 03:22:26 +0600

    updated source of component in expense tracker

--- mosh-react-course/src/expense-tracker/ExpenseTracker.tsx
+++ mosh-react-course/src/expense-tracker/ExpenseTracker.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import Form from "./components/Form";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+import Form from "../components/Form";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseFilter from "./components/ExpenseFilter";
+import ExpenseForm from "./components/ExpenseForm";
+import categories from "./categories";
 
 const ExpenseTracker = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
