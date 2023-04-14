981adbe731708e85cd236e9d48f4040d6a9e318f
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-15 03:40:19 +0600

    Effect Dependecies

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import ProductList from "./expense-tracker/components/ProductList";
 
 const App = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const [category, setCategory] = useState("");
 
   //afterRender
   useEffect(() => {
@@ -14,7 +16,17 @@ const App = () => {
 
   return (
     <div>
-      <input ref={ref} type="text" className="form-control" />
+      <select
+        className="form-select"
+        onChange={(event) => {
+          setCategory(event.target.value);
+        }}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 };
