07605a32ab7e21f4b2fafdbf50d9323d73579c18
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-15 15:28:34 +0600

    Working with async and await

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -7,14 +7,22 @@ interface User {
 }
 
 const App = () => {
-  const [users, seUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/ussers")
-      .then((res) => seUsers(res.data))
-      .catch((err) => setError(err.message));
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchUsers();
   }, []);
   return (
     <>
