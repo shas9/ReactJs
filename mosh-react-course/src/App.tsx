ce8ac2768484e2ef55023e7c1faf4b9315cd7e30
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-04-15 16:29:06 +0600

    Cancelling the fetch request

--- mosh-react-course/src/App.tsx
+++ mosh-react-course/src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 import React, { useEffect, useState } from "react";
 
 interface User {
@@ -11,18 +11,18 @@ const App = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(res.data);
-      } catch (err) {
+    const controller = new AbortController();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
         setError(err.message);
-      }
-    };
+      });
 
-    fetchUsers();
+    return () => controller.abort();
   }, []);
   return (
     <>
